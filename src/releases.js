f76f479671e0790c627d044c9b0705270c2a59de
Cofeiini <cofeiini@gmail.com>
2025-11-06 20:44:22 +0200

    Add version 1.1.3

--- src/releases.js
+++ src/releases.js
@@ -1,6 +1,18 @@
 // Remember to keep the latest release at the top
 export const RELEASES = [
     {
+        version: "1.1.3",
+        points: {
+            starting: 1,
+            leveling: 100,
+            questing: 25,
+            ascendancy: {
+                starting: 1,
+                questing: 8,
+            },
+        },
+    },
+    {
         version: "1.1.2",
         points: {
             starting: 1,
